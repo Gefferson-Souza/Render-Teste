1e292bb012c15ca4cbfabc99a71d07d86389b84c
Gefferson-Souza <geffersonteodorodesouza@gmail.com>
2023-02-24 15:11:05 -0300

    first commit

---
+++ part 2 vite/src/App.tsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import noteService from './services/Notes'
+
+function App() {
+
+  interface Inotes {
+    id: number;
+    content: String;
+    important: Boolean;
+  }
+
+  const [notes, setNotes] = useState<Inotes[]>([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  
+
+  const hook = () => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }
+  useEffect(hook, [])
+
+
+  
+
+  const submitNote = (e:any) =>{
+    e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  } 
+
+  const toggleImportantOf = (id:number) => {
+    const note:any = notes.find((n) => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => setNotes(notes.map((n) => n.id !== id ? n : returnedNote)))
+      .catch(error => {
+        alert (
+          `The note ${note.content} Was already deleted from the server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const deleteNote = (id:number) => {
+    const note = notes.find(n => n.id === id)
+
+    noteService
+    .del(id)
+    .then(deletedNote => setNotes(notes.filter(n => n.id !== id)))
+    .catch(error => {
+      alert (
+        `The note with the id: '${id}' Was already deleted from the server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
+
+  }
+
+  const allNotes = notes.map((note) =>{
+     return <li key={note.id} onClick={() => deleteNote(note.id)} >{note.content}</li>
+    })
+
+  const importantNotes = notes.map((note) => {
+    return note.important ?  <li key={note.id}  onClick={() => deleteNote(note.id)} >{note.content}</li> : null
+  })
+
+  return (
+    <div className="App">
+      <h1>Notes</h1>
+      <ul>{showAll ? allNotes : importantNotes }</ul>
+      <button onClick={() => setShowAll(!showAll)}>Show {showAll ?'Important':'All'}</button>
+      <form onSubmit={submitNote}>
+        <input value={newNote} onChange={(e) => setNewNote(e.target.value)}  />
+        <button>ADD Note</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part 2 vite/src/services/Notes.tsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseUrl:string = 'http://localhost:3005/api/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = (newObject:any) => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id:number, newObject:any) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const del = (id:number) => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, update, del }
