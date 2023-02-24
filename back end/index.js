a13ae84750ca8fb51da330ec35c65c716065d9e2
Gefferson-Souza <geffersonteodorodesouza@gmail.com>
2023-02-24 16:14:55 -0300

    second commit

--- back end/index.js
+++ back end/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ',request.method)
@@ -34,9 +35,6 @@ let notes = [
     }
   ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
--- part 2 vite/src/services/Notes.tsx
+++ part 2 vite/src/services/Notes.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl:string = 'http://localhost:3005/api/notes'
+const baseUrl:string = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
